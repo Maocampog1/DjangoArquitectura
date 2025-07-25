c4591a400a5d4df290852156f5b5f8b9f43db132
Maocampog1 <maocampog1@eafit.edu.co>
2025-07-25 14:37:21 -0500

    Actualizacion de views y nueva pagina de about-us

--- pages/views.py
+++ pages/views.py
@@ -3,5 +3,24 @@
 from django.shortcuts import render # here by default
 from django.views.generic import TemplateView
 
+cfrom django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import TemplateView
+
+# Create your views here.
 class HomePageView(TemplateView):
-    template_name = "pages/home.html"
+    template_name = 'pages/home.html'
+
+
+class AboutPageView(TemplateView):
+    template_name = 'pages/about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            "title": "About us - Online Store",
+            "subtitle": "About us",
+            "description": "This is an about page ...",
+            "author": "Developed by: Maria Alejandra Ocampo",
+        })
+        return context
