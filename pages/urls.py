83e26cd3b14ccc11a0e4271770ea11a99d0cf0e2
Maocampog1 <maocampog1@eafit.edu.co>
2025-08-07 07:06:18 -0500

    Agrega sistema de carrito para tutorial 03-A

--- pages/urls.py
+++ pages/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from .views import (
+    CartView,
     HomePageView,
     AboutPageView,
     ContactPageView,
     ProductIndexView,
     ProductShowView,
-    ProductCreateView
+    ProductCreateView,
+    CartRemoveAllView
 )
 
 urlpatterns = [
@@ -15,5 +17,9 @@ urlpatterns = [
     path('products/create', ProductCreateView.as_view(), name='form'),
     path('products/', ProductIndexView.as_view(), name='index'),
     path('products/<str:id>/', ProductShowView.as_view(), name='show'),
-    
-] 
+    path('cart/', CartView.as_view(), name='cart_index'),
+    path('cart/add/<int:product_id>/', CartView.as_view(), name='cart_add'),
+    path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'),
+
+
+]
