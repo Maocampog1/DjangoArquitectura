f7e000d9294ea769026d977e418ca11f8923b7f4
Maocampog1 <maocampog1@eafit.edu.co>
2025-08-30 14:19:17 -0500

    feat: implementación de la parte C- Other C.RU.D. operations with an API system  )

--- todoapp/backend/api/urls.py
+++ todoapp/backend/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ToDoListCreate
+from .views import ToDoListCreate, ToDoRetrieveUpdateDestroy
 
 urlpatterns = [
     path('todos/', ToDoListCreate.as_view(), name='todo_list'),
+    path('todos/<int:pk>/', ToDoRetrieveUpdateDestroy.as_view(), name='todo_detail'),
 ]
--- todoapp/backend/api/views.py
+++ todoapp/backend/api/views.py
@@ -2,16 +2,21 @@ from rest_framework import generics, permissions
 from .serializers import ToDoSerializer
 from todo.models import ToDo
 
+
 class ToDoListCreate(generics.ListCreateAPIView):
-    """
-    GET  /api/todos/  -> lista los ToDos del usuario autenticado
-    POST /api/todos/  -> crea un ToDo para el usuario autenticado
-    """
     serializer_class = ToDoSerializer
-    permission_classes = [permissions.IsAuthenticated]  # <- clave
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         return ToDo.objects.filter(user=self.request.user).order_by('-created')
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+
+class ToDoRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ToDoSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return ToDo.objects.filter(user=self.request.user)
